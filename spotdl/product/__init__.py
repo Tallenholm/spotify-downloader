ee49ecda30148c3f74e1d635e42b5f62a7df1c68
Tallenholm <99557318+Tallenholm@users.noreply.github.com>
2026-08-10 10:11:39 -0500

    feat: add product layer package

---
+++ spotdl/product/__init__.py
@@ -0,0 +1,17 @@
+"""Product-layer services for the enhanced spotDL experience."""
+
+from spotdl.product.models import (
+    AlbumEdition,
+    CandidateAssessment,
+    CandidateDecision,
+    ContentPreference,
+    EditionFamily,
+)
+
+__all__ = [
+    "AlbumEdition",
+    "CandidateAssessment",
+    "CandidateDecision",
+    "ContentPreference",
+    "EditionFamily",
+]
